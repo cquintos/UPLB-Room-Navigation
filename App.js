a272196a64fbdf2461cd85261070194ad60de0e5
cquintos <jcarlo.quintos@gmail.com>
2019-03-31 22:27:59 +0800

    Map view and get location function initialized

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import {MapView, Permissions, Location} from 'expo';
+import Menu from './app/components/Menu'
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: null,
+    }
+    this._getSyncLocationAsync();
+  }
+  _getSyncLocationAsync = async () => {
+    let {status} = await Permissions.askAsync(Permissions.LOCATION);
+      if(status !== "granted")
+        console.log('Permission Denied');
+      
+    let location = await Location.getCurrentPositionAsync({enabledHighAccuracy: true})
+    let region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.045,
+    }
+    this.setState({region: region})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MapView
+          initialRegion= {this.state.region}
+          showsUserLocation={true}
+          showCompasss= {true}    
+          rotateEnabled= {false}
+          style= {{flex: 1}}
+
+        />
+
       </View>
     );
   }
@@ -14,8 +50,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
